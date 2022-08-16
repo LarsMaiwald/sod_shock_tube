f91dd1e333cc9e994e17de3a81eefc41a37e26f6
Lars Maiwald <lars.maiwald@uni-jena.de>
2022-08-16 16:51:23 +0200

    more parameters and restructuring of loading

--- main.py
+++ main.py
@@ -18,5 +18,8 @@ import yaml
 # Setup
 # Loading the configuration
 cfg = yaml.safe_load(open('config.yml'))
-N_x, x_min, x_max, t_final = (cfg['N_x'], cfg['x_min'], cfg['x_max'],
-                              cfg['t_final'])
+N_x, t_final, CFL, g = cfg['N_x'], cfg['t_final'], cfg['CFL'], cfg['g']
+x_min, x_max = cfg['x_min'], cfg['x_max']
+u0_l, u0_r = cfg['u0_l'], cfg['u0_r']
+p0_l, p0_r = cfg['p0_l'], cfg['p0_r']
+rho0_l, rho0_r = cfg['r0_l'], cfg['r0_r']
